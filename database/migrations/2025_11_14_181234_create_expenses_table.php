04c7057fbe94c6d704cb8ec7ce17286d7fbce788
Matthias Claessen <u0157529@thomasmore.be>
2025-11-14 19:21:13 +0100

    Added Expense model and migration

---
+++ database/migrations/2025_11_14_181234_create_expenses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('expenses', function (Blueprint $table) {
+			$table->id();
+			$table->float('bedrag');
+			$table->string('categorie');
+			$table->string('omschrijving');
+			$table->string('status');
+			$table->string('afgekeurd_omschrijving');
+			$table->timestamps();
+		});
+	}
+	
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('expenses');
+	}
+	
+};
